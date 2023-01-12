0d4eaff045d7a769910015db22dc514dfcb962c4
camrohlof <camrohlof@gmail.com>
2023-01-12 16:50:09 -0500

    init-ish

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port");
+    run(listener)?.await
+}
