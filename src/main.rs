afa47594b401cd9cb9f5e40e88a70c748c4e556c
camrohlof <camrohlof@gmail.com>
2023-01-21 14:05:49 -0500

    Adds test db config, fixes tests

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,6 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
+
     run(listener, connection_pool)?.await
 }
